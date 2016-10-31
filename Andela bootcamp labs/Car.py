9efb84f734e46dfe5c55c214fc007192bae69c02
KENNEDY KOLUTE <kkolute@gmail.com>
2016-10-31 17:29:07 +0300

    edit

--- Andela bootcamp labs/Car.py
+++ Andela bootcamp labs/Car.py
@@ -1,32 +1,30 @@
 class Car(object):
-
-
-    def __init__(self,name='General', model='GM' ,type_of_car='saloon' ):
-        self.type_of_car = type_of_car
+    def __init__(self, name='General', model='GM', type='saloon'):
+        self.type = type
         self.model = model
         self.name = name
         self.speed = 0
 
-        if name== 'Porshe' or name== 'Koenigsegg':
+        if name == 'Porshe' or name == 'Koenigsegg':
             self.num_of_doors = 2
         else:
             self.num_of_doors = 4
 
-        if type_of_car == 'trailer':
+        if type == 'trailer':
             self.num_of_wheels = 8
         else:
             self.num_of_wheels = 4
 
     def drive(self, spd):
-        if self.type_of_car == 'trailer':
+        if self.type == 'trailer':
             self.speed = spd * 11
         else:
             self.speed = 10 ** spd
 
         return self
-        
+
     def is_saloon(self):
-        if self.type_of_car =='saloon':
+        if self.type == 'saloon':
             return 'True'
         else:
             return 'False'
