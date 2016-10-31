2e44847fce9103618c81746f79f636c3a827a69a
KENNEDY KOLUTE <kkolute@gmail.com>
2016-10-31 16:27:05 +0300

    oop real world problem

---
+++ Andela bootcamp labs/real world problem.py
@@ -0,0 +1,60 @@
+class BankAccount(object):
+    def withdraw(self, amount):
+        pass
+
+    def deposit(self, amount):
+        pass
+
+
+class SavingsAccount(BankAccount):
+    def __init__(self):
+        self.balance = 500
+        self.CreditLine = 500
+
+    def withdraw(self, amount):
+        if (self.balance - amount < -self.CreditLine):
+            return "Cannot withdraw beyond the current account balance"
+        elif (self.balance < amount):
+            return "Cannot withdraw beyond the current account balance"
+        elif (amount < 0):
+            return "Invalid withdraw amount"
+        else:
+            self.balance -= amount
+            return self.balance
+
+    def deposit(self, amount):
+
+        if (amount < 0):
+            return "Invalid deposit amount"
+        else:
+            self.balance += amount
+            return self.balance
+
+
+class CurrentAccount(BankAccount):
+    def __init__(self):
+        self.balance = 0
+
+    def deposit(self, amount):
+        if (amount < 0):
+            return "Invalid deposit amount"
+        else:
+            self.balance += amount
+            return self.balance
+
+    def withdraw(self, amount):
+        if (self.balance < amount):
+            return "Cannot withdraw beyond the current account balance"
+        elif (amount < 0):
+            return "Invalid withdraw amount"
+        else:
+            self.balance -= amount
+            return self.balance
+
+
+sa = SavingsAccount()
+
+print(sa.withdraw(5000))
+print(sa.balance)
+sa.balance = 9
+print(sa.balance)
