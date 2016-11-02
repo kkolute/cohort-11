fa3c691ec360f05adcdb375600ab7900dd5309a9
KENNEDY KOLUTE <kkolute@gmail.com>
2016-11-02 17:04:00 +0300

    edited

--- Andela bootcamp labs/factorial.py
+++ Andela bootcamp labs/factorial.py
@@ -1,5 +1,11 @@
+'''
+author : Kennedy kolute
+class  : Andela bootcamp 11
+'''
+
 def factorial(number):
     if number == 0:
         return 1
     else:
+	# use recursion to generate the factorial
         return number * factorial( number -1)
